2bff82b0c86518ba22b8efccdeaefb9d9157365f
zhangtao101 <34822654+zhangtao101@users.noreply.github.com>
2026-07-07 16:55:25 +0800

    chore(config): switch API to hzsmt-user + baseURL 192.168.0.211

--- api/request.js
+++ api/request.js
@@ -2,14 +2,13 @@
 // const baseURL = 'http://192.168.0.102:8060/'
 // const baseURL = 'http://192.168.0.14:8050/'
 // const baseURL = 'http://192.168.0.15:8050/'
-const baseURL = 'http://10.10.0.220:8060/'
+// const baseURL = 'http://10.10.0.220:8060/'
 // const baseURL = 'http://192.168.10.2:8060/'
 // const baseURL = 'http://192.168.0.61:8050/'
- // const baseURL = 'http://192.168.0.211:8060/'
+ const baseURL = 'http://192.168.0.211:8060/'
 // const baseURL = 'http://192.168.0.13:8050/'
 // const baseURL = 'http://192.168.79.115:8060/'
 // const baseURL = 'http://192.168.31.60:8060/'
-// const baseURL = 'http://192.168.0.211:8186/'
 
 const request = (o) => {
 	const auth = uni.getStorageSync('auth');
